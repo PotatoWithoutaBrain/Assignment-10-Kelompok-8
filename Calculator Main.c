3c944cb6bca56b03b9ede51acf4a7902eea055c9
PotatoWithoutaBrain <kenturboxyz@gmail.com>
2026-06-08 00:55:20 +0700

    Add files via upload

---
+++ Calculator Main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    int a,b,pilih;
+    printf("--- Kalkulator ---\nMasukkan Angka Pertama: ");
+    scanf("%i",&a);
+    printf("Masukkan Angka Kedua: ");
+    scanf("%i",&b);
+    printf("Pilih Operasi: \n1. Tambah\n2. Kurang\n3. Bagi\n4. Kali\nPilihan: ");
+    scanf("%i",&pilih);
+    switch (pilih) {
+        case 1:
+            
+    }
+}
