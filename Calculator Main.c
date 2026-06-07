61ce25f531e0c1da0c209dbb834a3f3ecb72f5bf
ichiholdings <maxwell.biggame@gmail.com>
2026-06-08 01:00:40 +0700

    menambahkan fungsi tambah + default case

--- Calculator Main.c
+++ Calculator Main.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
 int main () {
-    int a,b,pilih;
+    int a, b, hasil, pilih;
     printf("--- Kalkulator ---\nMasukkan Angka Pertama: ");
     scanf("%i",&a);
     printf("Masukkan Angka Kedua: ");
     scanf("%i",&b);
     printf("Pilih Operasi: \n1. Tambah\n2. Kurang\n3. Bagi\n4. Kali\nPilihan: ");
     scanf("%i",&pilih);
+
     switch (pilih) {
         case 1:
-            
+            printf("Angka pertama: %d\n", a);
+            printf("Angka kedua: %d\n", b);
+            hasil = a + b;
+            printf("Hasil operasi: %d\n", hasil);
+            break;
+        default:
+            printf("Pilihan tidak valid!\n");
+            break;
     }
 }
